ccf84751645e1ceea7c8ef95214b60b3e7ae890d
Brendon Tse and Mia Murdock <brendon@>
2019-06-04 17:05:29 -0700

    add everything

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".js").click(function() {
+    $(".js-term").slideToggle();
+    $(".js-answer").slideToggle();
+  });
+  $(".11").click(function() {
+    $(".js-term2").slideToggle();
+    $(".js-answer2").slideToggle();
+  });
+});
